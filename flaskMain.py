eda68b72c6b2ae2283179e6ae1f45fcd9ff8b16d
jg <57115019+joeyagreco@users.noreply.github.com>
2020-10-31 15:22:58 -0500

    added subtitle variable

--- flaskMain.py
+++ flaskMain.py
@@ -12,14 +12,15 @@ def index():
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
+    subtitle = "Test"
     try:
         c = Calculator.Calculator()
         num1 = request.form["num1"]
         num2 = request.form["num2"]
         sum = c.add(num1, num2)
-        return render_template("test.html", subtitle="Test", sum=sum)
+        return render_template("test.html", subtitle=subtitle, sum=sum)
     except:
-        return render_template("test.html", subtitle="Test", sum="N/A")
+        return render_template("test.html", subtitle=subtitle, sum="N/A")
 
 
 if __name__ == "__main__":
