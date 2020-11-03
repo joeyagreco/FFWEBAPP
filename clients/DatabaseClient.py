534f6dcc72672deb25b8c4179f696c012ccaa0d5
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-02 20:05:19 -0600

    fixed instance names

--- clients/DatabaseClient.py
+++ clients/DatabaseClient.py
@@ -7,5 +7,5 @@ class DatabaseClient:
     def __init__(self, leagueId):
         self.__leagueId = leagueId
         self.__cluster = MongoClient("mongodb://127.0.0.1:27017/")
-        self.__database = self.cluster["TestDatabase"]
-        self.__collection = self.db["TestCollection"]
+        self.__database = self.__cluster["TestDatabase"]
+        self.__collection = self.__database["TestCollection"]
