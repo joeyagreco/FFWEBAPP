85e2ed7537ffc11c34717baa066277acd803475b
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-02 20:32:35 -0600

    added models

---
+++ models/TeamStatsModel.py
@@ -0,0 +1,39 @@
+class TeamStatsModel:
+
+    def __init__(self, ppg: float, stddev: float, awal: float, rawTeamScore: float, rawTeamSuccess: float, rawTeamLuck: int, plusMinus: float, maxScore: float, minScore: float):
+        self.__ppg = ppg
+        self.__stddev = stddev
+        self.__awal = awal
+        self.__rawTeamScore = rawTeamScore
+        self.__rawTeamSuccess = rawTeamSuccess
+        self.__rawTeamLuck = rawTeamLuck
+        self.__plusMinus = plusMinus
+        self.__maxScore = maxScore
+        self.__minScore = minScore
+
+    def getPpg(self):
+        return self.__ppg
+
+    def getStddev(self):
+        return self.__stddev
+
+    def getAwal(self):
+        return self.__awal
+
+    def getRawTeamScore(self):
+        return self.__rawTeamScore
+
+    def getRawTeamSuccess(self):
+        return self.__rawTeamSuccess
+
+    def getRawTeamLuck(self):
+        return self.__rawTeamLuck
+
+    def getPlusMinus(self):
+        return self.__plusMinus
+
+    def getMaxScore(self):
+        return self.__maxScore
+
+    def getMinScore(self):
+        return self.__minScore
