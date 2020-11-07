e323fba630fa905b6398a1df4007d4eba5c89d9c
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-07 10:09:41 -0600

    non working js

--- static/javascript/leagueHomepage.js
+++ static/javascript/leagueHomepage.js
@@ -1,7 +1,8 @@
 function Copy() {
-    //var Url = document.createElement("textarea");
-    urlCopied.innerHTML = window.location.href;
-    //Copied = Url.createTextRange();
-    //Copied.execCommand("Copy");
+  var url = document.getElementById("league_id");
+  url.value = window.location.href;
+  url.focus();
+  url.select();
+  document.execCommand("Copy");
 }
-            }
+
