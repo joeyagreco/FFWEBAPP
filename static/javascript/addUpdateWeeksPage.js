07ad8a127d4d0b8c92e6660d90e68e5ee22f6c8d
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-19 19:26:44 -0600

    added functionality for dropdown selection of week to change week shown

--- static/javascript/addUpdateWeeksPage.js
+++ static/javascript/addUpdateWeeksPage.js
@@ -1,5 +1,10 @@
 function addWeek() {
-    console.log("in js function");
     leagueId = document.getElementById("league_id").value;
     window.location = "/add-week?league_id="+leagueId;
+}
+
+function updateWeekDropdown() {
+    leagueId = document.getElementById("league_id").value;
+    newWeek = document.getElementById("select_week_dropdown").value;
+    window.location = "/add-update-weeks?league_id="+leagueId+"&week="+newWeek;
 }
