e07a24f1a7e3da5d3704b0ee1c834e8646506ba5
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-20 17:59:35 -0600

    deleted disable button function

--- static/javascript/addUpdateWeeksPage.js
+++ static/javascript/addUpdateWeeksPage.js
@@ -1,21 +1,16 @@
-window.onload = function disableButtonsIfNecessary() {
-
-    document.getElementById("delete_week_button").disabled = true;
-}
-
 function addWeek() {
-    leagueId = document.getElementById("league_id").value;
+    var leagueId = document.getElementById("league_id").value;
     window.location = "/add-week?league_id="+leagueId;
 }
 
 function updateWeekDropdown() {
-    leagueId = document.getElementById("league_id").value;
-    newWeek = document.getElementById("select_week_dropdown").value;
+    var leagueId = document.getElementById("league_id").value;
+    var newWeek = document.getElementById("select_week_dropdown").value;
     window.location = "/add-update-weeks?league_id="+leagueId+"&week="+newWeek;
 }
 
 function deleteWeek() {
-    leagueId = document.getElementById("league_id").value;
-    week = document.getElementById("select_week_dropdown").value;
+    var leagueId = document.getElementById("league_id").value;
+    var week = document.getElementById("select_week_dropdown").value;
     window.location = "/delete-week?league_id="+leagueId+"&week="+week;
 }
