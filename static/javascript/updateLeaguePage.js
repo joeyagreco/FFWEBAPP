7130f340f3e044cf8f81cf06000e274c8f17d033
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-23 11:07:22 -0600

    removed popup for league successfully updated

--- static/javascript/updateLeaguePage.js
+++ static/javascript/updateLeaguePage.js
@@ -1,18 +1,14 @@
 function deleteLeague() {
     // GET request
-    leagueId = document.getElementById("league_id").value;
+    var leagueId = document.getElementById("league_id").value;
     if(confirm("Are you sure you want to delete your league?")){
         window.location = "/delete-league?league_id="+leagueId;
     }
 }
 
-function successfulUpdatePopup() {
-    alert("League Successfully Updated");
-}
-
 function sendToUpdateWeeks() {
     // GET request
-    leagueId = document.getElementById("league_id").value;
+    var leagueId = document.getElementById("league_id").value;
     window.location = "/add-update-weeks?league_id="+leagueId;
 
 }
