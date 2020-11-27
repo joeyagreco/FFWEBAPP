60012b0a3ba221460e364db4def0b753f4341270
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-27 15:18:31 -0600

    clean up imports and print out

--- services/StatCalculatorService.py
+++ services/StatCalculatorService.py
@@ -1,6 +1,6 @@
 from models.league_models.LeagueModel import LeagueModel
 from packages.StatCalculators.MinMaxScoreCalculator import MinMaxScoreCalculator
-from packages.StatCalculators.PpgCalculator import Ppg
+from packages.StatCalculators.PpgCalculator import PpgCalculator
 
 
 class StatCalculatorService:
@@ -13,11 +13,13 @@ class StatCalculatorService:
         Returns a list of TeamStatsModels, one for each team in the given league.
         """
         for team in self.__leagueModel.getTeams():
+            print(f"Team ID: {team.getTeamId()}")
             minMaxScoreCalculator = MinMaxScoreCalculator(team.getTeamId(), self.__leagueModel)
-            print(minMaxScoreCalculator.getMinScore())
-            print(minMaxScoreCalculator.getMaxScore())
-            ppgCalculator = Ppg(team.getTeamId(), self.__leagueModel)
-            print(ppgCalculator.getPpg())
+            print(f"Min Score: {minMaxScoreCalculator.getMinScore()}")
+            print(f"Max Score: {minMaxScoreCalculator.getMaxScore()}")
+            ppgCalculator = PpgCalculator(team.getTeamId(), self.__leagueModel)
+            print(f"PPG: {ppgCalculator.getPpg()}")
+            print()
 
     def getLeagueStats(self):
         """
