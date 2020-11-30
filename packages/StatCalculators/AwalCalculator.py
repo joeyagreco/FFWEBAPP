53bb592b603135d86f74fd82633a04b734fb1099
jg <57115019+joeyagreco@users.noreply.github.com>
2020-11-30 17:36:26 -0600

    working on adding awal calculator and tests

---
+++ packages/StatCalculators/AwalCalculator.py
@@ -0,0 +1,104 @@
+from models.league_models.LeagueModel import LeagueModel
+from models.league_models.WeekModel import WeekModel
+
+
+class AwalCalculator:
+
+    def __init__(self, teamId: int, leagueModel: LeagueModel):
+        self.__teamId = teamId
+        self.__leagueModel = leagueModel
+
+    def getAwal(self):
+        """
+        Returns a float that is the AWAL for the team with the given ID.
+        A = W * (1/L) + T * (0.5/L) - WAL
+        Where:
+        WAL = Game outcome (1=win, 0=loss, 0.5=tie)
+        W = Teams outscored
+        T = Teams tied
+        L = Opponents in league (league size - 1)
+        """
+        totalAdjustment = 0
+        totalWal = 0
+        L = self.__leagueModel.getNumberOfTeams() - 1
+        for week in self.__leagueModel.getWeeks():
+            WAL = self.__getTeamOutcomeOfWeek(week)
+            W = self.__getTeamsOutscoredOfWeek(week)
+            T = self.__getTeamsTiedOfWeek(week)
+            A = W * (1/L) + T * (0.5/L) - WAL
+            totalAdjustment += A
+            totalWal += WAL
+            print(f"Team {self.__teamId}")
+            print(f"WAL: {WAL}")
+            print(f"W: {W}")
+            print(f"T: {T}")
+            print(f"A: {A}")
+        print(f"Total Adjustment: {totalAdjustment}")
+        print(f"Total WAL: {totalWal}")
+        return totalAdjustment + totalWal
+
+    def __getTeamOutcomeOfWeek(self, week: WeekModel):
+        """
+        Returns the outcome in the given week for the team with self.__teamId
+        (1=win, 0=loss, 0.5=tie)
+        """
+        for matchup in week.getMatchups():
+            if matchup.getTeamA().getTeamId() == self.__teamId:
+                # our target team is TeamA
+                if matchup.getTeamAScore() > matchup.getTeamBScore():
+                    # our target team won
+                    return 1
+                elif matchup.getTeamAScore() < matchup.getTeamBScore():
+                    # our target team lost
+                    return 0
+                else:
+                    # our target team tied
+                    return 0.5
+            elif matchup.getTeamB().getTeamId() == self.__teamId:
+                # our target team is TeamB
+                if matchup.getTeamBScore() > matchup.getTeamAScore():
+                    # our target team won
+                    return 1
+                elif matchup.getTeamBScore() < matchup.getTeamAScore():
+                    # our target team lost
+                    return 0
+                else:
+                    # our target team tied
+                    return 0.5
+
+    def __getTeamsOutscoredOfWeek(self, week: WeekModel):
+        """
+        Returns the number of teams outscored in the given week for the team with self.__teamId
+        """
+        allScores = {}
+        for matchup in week.getMatchups():
+            allScores[matchup.getTeamA().getTeamId()] = matchup.getTeamAScore()
+            allScores[matchup.getTeamB().getTeamId()] = matchup.getTeamBScore()
+        allScoresList = []
+        for teamId in allScores:
+            allScoresList.append(allScores[teamId])
+        targetTeamScore = allScores[self.__teamId]
+        teamsOutscored = 0
+        for score in allScoresList:
+            if targetTeamScore > score:
+                teamsOutscored += 1
+        return teamsOutscored
+
+    def __getTeamsTiedOfWeek(self, week: WeekModel):
+        """
+        Returns the number of teams tied in the given week for the team with self.__teamId
+        """
+        allScores = {}
+        for matchup in week.getMatchups():
+            allScores[matchup.getTeamA().getTeamId()] = matchup.getTeamAScore()
+            allScores[matchup.getTeamB().getTeamId()] = matchup.getTeamBScore()
+        allScoresList = []
+        for teamId in allScores:
+            allScoresList.append(allScores[teamId])
+        targetTeamScore = allScores[self.__teamId]
+        teamsTied = 0
+        print(allScoresList)
+        for score in allScoresList:
+            if targetTeamScore == score:
+                teamsTied += 1
+        return teamsTied
---
+++ test/test_StatCalculators/test_AwalCalculator.py
@@ -0,0 +1,30 @@
+import unittest
+
+from models.league_models.LeagueModel import LeagueModel
+from models.league_models.MatchupModel import MatchupModel
+from models.league_models.TeamModel import TeamModel
+from models.league_models.WeekModel import WeekModel
+from packages.StatCalculators.AwalCalculator import AwalCalculator
+from packages.StatCalculators.PpgCalculator import PpgCalculator
+
+
+class TestAwalCalculator(unittest.TestCase):
+
+    def test_getAwal(self):
+        team1 = TeamModel(1, "team1")
+        team2 = TeamModel(2, "team2")
+        team3 = TeamModel(3, "team3")
+        team4 = TeamModel(4, "team4")
+        team5 = TeamModel(5, "team5")
+        team6 = TeamModel(6, "team6")
+        teamList = [team1, team2, team3, team4, team5, team6]
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 101)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        weekList = [week1]
+        leagueModel = LeagueModel(123456, "test", 6, teamList, weekList)
+        awalTeam1 = AwalCalculator(1, leagueModel).getAwal()
+        #self.assertIsInstance(awalTeam1, float)
+        self.assertEqual(0.2, awalTeam1)
