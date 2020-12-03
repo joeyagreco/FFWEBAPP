209a6b8f5f96af58c5ab0e9b3848f95240bdc4c6
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-03 17:32:18 -0600

    added button to base that will take user back to league homepage

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -1,3 +1,8 @@
 function homeRedirect() {
     window.location = "/";
 }
+
+function leagueHomepageRedirect() {
+    var leagueId = document.getElementById("league_id").value;
+    window.location = "/league-homepage?league_id=" + leagueId;
+}
