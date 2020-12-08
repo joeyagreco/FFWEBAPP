dd7c4e9a32c8e731c0b3e41a19e5a72a81aeb828
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-08 16:51:40 -0600

    added UNTESTED method in Rounder class

--- helpers/Rounder.py
+++ helpers/Rounder.py
@@ -1,5 +1,7 @@
 import math
 
+from models.league_models.LeagueModel import LeagueModel
+
 
 class Rounder:
 
@@ -15,3 +17,22 @@ class Rounder:
         else:
             part = math.floor(part)
         return part / (10 ** decimalPlaces)
+
+    def getDecimalPlacesRoundedToInScores(self, league: LeagueModel):
+        """
+        Returns an int that represents how many decimal places the scores in this league are rounded to.
+        Ex: score of 132.55 -> 2, score of 130.10 -> 1
+        """
+
+        maxDecimalPlaces = 0
+        for week in league.getWeeks():
+            for matchup in week.getMatchups():
+                aScoreStr = str(matchup.getTeamAScore())
+                bScoreStr = str(matchup.getTeamBScore())
+                aScoreDecimalLength = len(aScoreStr.split(".")[1])
+                bScoreDecimalLength = len(bScoreStr.split(".")[1])
+                testDecimalLength = max(aScoreDecimalLength, bScoreDecimalLength)
+                if testDecimalLength > maxDecimalPlaces:
+                    maxDecimalPlaces = testDecimalLength
+        return maxDecimalPlaces
+
