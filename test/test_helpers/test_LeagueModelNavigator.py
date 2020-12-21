54c50585eb3fd5ec2025f55e6f5be5c40553a0a3
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-21 17:40:55 -0600

    added tests for league model navigator methods

--- test/test_helpers/test_LeagueModelNavigator.py
+++ test/test_helpers/test_LeagueModelNavigator.py
@@ -93,6 +93,47 @@ class TestLeagueModelNavigator(unittest.TestCase):
         self.assertEqual(1, team2GamesPlayed)
         self.assertEqual(0, nonTeamGamesPlayed)
 
+    def test_totalLeaguePoints(self):
+        team1 = TeamModel(1, "team1")
+        team2 = TeamModel(2, "team2")
+        team3 = TeamModel(3, "team3")
+        team4 = TeamModel(4, "team4")
+        team5 = TeamModel(5, "team5")
+        team6 = TeamModel(6, "team6")
+        teamList = [team1, team2, team3, team4, team5, team6]
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 101)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        weekList = [week1]
+        leagueModel = LeagueModel(123456, "test", 6, teamList, weekList)
+
+        leagueModelNavigator = LeagueModelNavigator()
+        totalLeaguePoints = leagueModelNavigator.totalLeaguePoints(leagueModel)
+        self.assertEqual(510.5, totalLeaguePoints)
+
+    def test_totalPointsScoredByTeam(self):
+        team1 = TeamModel(1, "team1")
+        team2 = TeamModel(2, "team2")
+        team3 = TeamModel(3, "team3")
+        team4 = TeamModel(4, "team4")
+        team5 = TeamModel(5, "team5")
+        team6 = TeamModel(6, "team6")
+        teamList = [team1, team2, team3, team4, team5, team6]
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 101)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        weekList = [week1]
+        leagueModel = LeagueModel(123456, "test", 6, teamList, weekList)
+
+        leagueModelNavigator = LeagueModelNavigator()
+        totalTeam1Points = leagueModelNavigator.totalPointsScoredByTeam(leagueModel, 1)
+        self.assertEqual(100, totalTeam1Points)
+
+
 
 
 
