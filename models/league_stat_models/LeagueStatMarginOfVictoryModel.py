345c3b6069b3bd39d1408106870e3a395f1d21ec
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-26 12:19:18 -0600

    updated MOV model

--- models/league_stat_models/LeagueStatMarginOfVictoryModel.py
+++ models/league_stat_models/LeagueStatMarginOfVictoryModel.py
@@ -1,9 +1,11 @@
 class LeagueStatMarginOfVictoryModel:
 
-    def __init__(self, marginOfVictory: float, winningTeamName: str, losingTeamName: str, week: int):
+    def __init__(self, marginOfVictory: float, winningTeamName: str, winningTeamPoints: float, losingTeamName: str, losingTeamPoints: float, week: int):
         self.__marginOfVictory = marginOfVictory
         self.__winningTeamName = winningTeamName
+        self.__winningTeamPoints = winningTeamPoints
         self.__losingTeamName = losingTeamName
+        self.__losingTeamPoints = losingTeamPoints
         self.__week = week
 
     def getMarginOfVictory(self):
@@ -12,8 +14,14 @@ class LeagueStatMarginOfVictoryModel:
     def getWinningTeamName(self):
         return self.__winningTeamName
 
+    def getWinningTeamPoints(self):
+        return self.__winningTeamPoints
+
     def getLosingTeamName(self):
         return self.__losingTeamName
+
+    def getLosingTeamPoints(self):
+        return self.__losingTeamPoints
 
     def getWeek(self):
         return self.__week
