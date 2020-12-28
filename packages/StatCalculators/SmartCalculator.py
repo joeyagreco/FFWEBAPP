cebcfe3eab36b4b5b5bc661a2bd3ed70fdbc7e46
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-28 17:55:57 -0600

    finished making and testing percentile

--- packages/StatCalculators/SmartCalculator.py
+++ packages/StatCalculators/SmartCalculator.py
@@ -1,3 +1,5 @@
+from helpers.LeagueModelNavigator import LeagueModelNavigator
+from helpers.Rounder import Rounder
 from models.league_models.LeagueModel import LeagueModel
 
 
@@ -9,10 +11,27 @@ class SmartCalculator:
     def getPercentileOfScore(self, score: float):
         """
         Returns the percentile of which this score would rank in self.__leagueModel.
+        This is the percentage of games this score would win if it played against every other score.
         Note: This assumes that the given score already exists in self.__leagueModel.
         """
+        leagueModelNavigator = LeagueModelNavigator()
+        rounder = Rounder()
+        # round the given score properly
+        decimalPlacesToRoundTo = rounder.getDecimalPlacesRoundedToInScores(self.__leagueModel)
+        score = rounder.normalRound(score, decimalPlacesToRoundTo)
         scoresBeat = 0
         scoresTied = 0
         totalScores = 0
-
-        return None
+        allScores = leagueModelNavigator.getAllScoresInLeague(self.__leagueModel)
+        for s in allScores:
+            totalScores += 1
+            if score > s:
+                scoresBeat += 1
+            elif score == s:
+                scoresTied += 1
+        # don't include *this* score in with the other scores tied or total scores(the score will always find and tie itself)
+        scoresTied -= 1
+        totalScores -= 1
+        rawPercentile = (scoresBeat + (scoresTied * 0.5)) / totalScores
+        percentile = rounder.normalRound(rawPercentile, 1) * 100
+        return percentile
---
+++ test/test_StatCalculators/test_SmartCalculator.py
@@ -0,0 +1,32 @@
+import unittest
+
+from models.league_models.LeagueModel import LeagueModel
+from models.league_models.MatchupModel import MatchupModel
+from models.league_models.TeamModel import TeamModel
+from models.league_models.WeekModel import WeekModel
+from packages.StatCalculators.SmartCalculator import SmartCalculator
+
+
+class TestSmartCalculator(unittest.TestCase):
+
+    def test_getPercentileOfScore(self):
+        team1 = TeamModel(1, "team1")
+        team2 = TeamModel(2, "team2")
+        team3 = TeamModel(3, "team3")
+        team4 = TeamModel(4, "team4")
+        team5 = TeamModel(5, "team5")
+        team6 = TeamModel(6, "team6")
+        teamList = [team1, team2, team3, team4, team5, team6]
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        weekList = [week1]
+        leagueModel = LeagueModel(123456, "test", 6, teamList, weekList)
+        smartCalculator = SmartCalculator(leagueModel)
+        percentile1 = smartCalculator.getPercentileOfScore(100)
+        percentile2 = smartCalculator.getPercentileOfScore(105)
+        self.assertIsInstance(percentile1, float)
+        self.assertEqual(30, percentile1)
+        self.assertEqual(100, percentile2)
