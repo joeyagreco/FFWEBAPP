6e64677b0fff7f57bc35df6911f35556b727776a
jg <57115019+joeyagreco@users.noreply.github.com>
2020-12-28 12:55:55 -0600

    formatting

--- static/javascript/leagueStatsPage.js
+++ static/javascript/leagueStatsPage.js
@@ -2,4 +2,4 @@ function submitLeagueStat() {
     var leagueId = document.getElementById("league_id").value;
     var leagueStat = document.getElementById("league_stat").value;
     window.location = "/league-stats?league_id="+leagueId+"&stat_selection="+leagueStat;
-    }
+}
