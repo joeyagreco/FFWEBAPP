af118a3aab62082e175f679b8c1734e56ff4b5f6
jg <57115019+joeyagreco@users.noreply.github.com>
2021-01-01 12:55:49 -0600

    added test for smart luck

--- test/test_StatCalculators/test_SmartCalculator.py
+++ test/test_StatCalculators/test_SmartCalculator.py
@@ -53,3 +53,23 @@ class TestSmartCalculator(unittest.TestCase):
         self.assertIsInstance(smartWins1, float)
         self.assertEqual(0.3, smartWins1)
 
+    def test_getSmartLuckOfScores(self):
+        team1 = TeamModel(1, "team1")
+        team2 = TeamModel(2, "team2")
+        team3 = TeamModel(3, "team3")
+        team4 = TeamModel(4, "team4")
+        team5 = TeamModel(5, "team5")
+        team6 = TeamModel(6, "team6")
+        teamList = [team1, team2, team3, team4, team5, team6]
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        weekList = [week1]
+        leagueModel = LeagueModel(123456, "test", 6, teamList, weekList)
+        smartCalculator = SmartCalculator(leagueModel)
+        smartLuck1 = smartCalculator.getSmartLuckOfScores([100], 0)
+        self.assertIsInstance(smartLuck1, float)
+        self.assertEqual(-0.3, smartLuck1)
+
