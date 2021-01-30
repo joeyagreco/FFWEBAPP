855eca866eaef82729d00cf7666954a3a8f99b23
jg <57115019+joeyagreco@users.noreply.github.com>
2021-01-29 20:57:42 -0600

    no longer accept vs team ids param

--- packages/StatCalculators/StrengthOfScheduleCalculator.py
+++ packages/StatCalculators/StrengthOfScheduleCalculator.py
@@ -16,11 +16,9 @@ class StrengthOfScheduleCalculator:
         Returns the Strength of Schedule that the team with the given id has had.
         THROUGHWEEK: [int] Gives SoS through that week.
         ONLYWEEKS: [list] Gives SoS for the given week numbers.
-        VSTEAMIDS: [list] Gives SoS vs teams with the given IDs.
         """
         throughWeek = params.pop("throughWeek", LeagueModelNavigator.getNumberOfWeeksInLeague(self.__leagueModel))
         onlyWeeks = params.pop("onlyWeeks", None)
-        vsTeamIds = params.pop("vsTeamIds", LeagueModelNavigator.getAllTeamIdsInLeague(self.__leagueModel, excludeId=self.__teamId))
         totalOpponentAwal = 0
         gameCount = 0
         for week in self.__leagueModel.getWeeks():
@@ -29,12 +27,12 @@ class StrengthOfScheduleCalculator:
             elif week.getWeekNumber() > throughWeek:
                 break
             for matchup in week.getMatchups():
-                if matchup.getTeamA().getTeamId() == self.__teamId and matchup.getTeamB().getTeamId() in vsTeamIds:
+                if matchup.getTeamA().getTeamId() == self.__teamId:
                     recordCalculator = RecordCalculator(matchup.getTeamB().getTeamId(), self.__leagueModel)
                     awalCalculator = AwalCalculator(matchup.getTeamB().getTeamId(), self.__leagueModel, recordCalculator.getWins(throughWeek=throughWeek), recordCalculator.getTies(throughWeek=throughWeek))
                     totalOpponentAwal += awalCalculator.getAwal(throughWeek=throughWeek)
                     gameCount += 1
-                elif matchup.getTeamB().getTeamId() == self.__teamId and matchup.getTeamA().getTeamId() in vsTeamIds:
+                elif matchup.getTeamB().getTeamId() == self.__teamId:
                     recordCalculator = RecordCalculator(matchup.getTeamA().getTeamId(), self.__leagueModel)
                     awalCalculator = AwalCalculator(matchup.getTeamA().getTeamId(), self.__leagueModel, recordCalculator.getWins(throughWeek=throughWeek), recordCalculator.getTies(throughWeek=throughWeek))
                     totalOpponentAwal += awalCalculator.getAwal(throughWeek=throughWeek)
