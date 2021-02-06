b3a282189bb18df15df192ad9633d2bb2c5716aa
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-05 19:48:38 -0600

    hardcoding AWAL in for now

--- static/javascript/statsBase.js
+++ static/javascript/statsBase.js
@@ -3,3 +3,9 @@ function submitStat() {
     var selectedStat = document.getElementById("stat_name").value;
     window.location = "/stats-explained?league_id="+leagueId+"&selected_stat="+selectedStat;
 }
+
+function reroute(statName) {
+    var leagueId = document.getElementById("league_id").value;
+    var selectedStat = statName;
+    window.location = "/stats-explained?league_id="+leagueId+"&selected_stat="+selectedStat;
+}
