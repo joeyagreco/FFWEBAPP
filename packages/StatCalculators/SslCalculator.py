e8459420d73dd026cf0189637512a41e3aa145d2
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-20 10:04:01 -0600

    updated team colors. also updated SSL formulas with rounding

--- packages/StatCalculators/SslCalculator.py
+++ packages/StatCalculators/SslCalculator.py
@@ -17,29 +17,36 @@ class SslCalculator:
         self.__pointsMultiplier = 20
         self.__minMaxMultiplier = 0.1
 
-    def getTeamScore(self) -> float:
+    def getTeamScore(self, **params) -> float:
         """
         Returns the Team Score that the team with the given stats has.
+        ROUNDED: [boolean] If false, do not round the return value. Defaults to True.
         Team Score = ((AWAL / G) * 100) + (Scoring Share * 0.2) + ((Max Score + Min Score) * 0.1)
         WHERE:
         G = Total games played by a team
         """
         teamScore = ((self.__awal / self.__gamesPlayed) * self.__awalMultiplier) + ((self.__totalTeamPoints / self.__totalLeaguePoints) * self.__pointsMultiplier) + ((self.__maxScore + self.__minScore) * self.__minMaxMultiplier)
-        return Rounder.normalRound(teamScore, 1)
+        rounded = params.pop("rounded", True)
+        if rounded:
+            return Rounder.normalRound(teamScore, 2)
+        return teamScore
 
-    def getTeamSuccess(self) -> float:
+    def getTeamSuccess(self, **params) -> float:
         """
         Returns the Team Success that the team with the given stats has.
+        ROUNDED: [boolean] If false, do not round the return value. Defaults to True.
         Team Success = ((WAL / G) * 100) + (Scoring Share * 0.2) + ((Max Score + Min Score) * 0.1)
         WHERE:
         G = Total games played by a team
         """
         teamSuccess = ((self.__wal / self.__gamesPlayed) * self.__walMultiplier) + ((self.__totalTeamPoints / self.__totalLeaguePoints) * self.__pointsMultiplier) + ((self.__maxScore + self.__minScore) * self.__minMaxMultiplier)
-
-        return Rounder.normalRound(teamSuccess, 1)
+        rounded = params.pop("rounded", True)
+        if rounded:
+            return Rounder.normalRound(teamSuccess, 2)
+        return teamSuccess
 
     def getTeamLuck(self) -> float:
         """
         Returns the Team Luck that the team with the given stats has.
         """
-        return Rounder.normalRound(self.getTeamSuccess() - self.getTeamScore(), 1)
+        return Rounder.normalRound(self.getTeamSuccess(rounded=False) - self.getTeamScore(rounded=False), 2)
--- test/test_StatCalculators/test_SslCalculator.py
+++ test/test_StatCalculators/test_SslCalculator.py
@@ -10,18 +10,18 @@ class TestSslCalculator(unittest.TestCase):
         rawTeamScoreTeam2 = SslCalculator(4.5, 4.5, 550.0, 150.0, 100.0, 7, 3600.0).getTeamScore()
         rawTeamScoreTeam3 = SslCalculator(2.5, 4.5, 550.0, 150.0, 100.0, 7, 3600.0).getTeamScore()
         self.assertIsInstance(rawTeamScoreTeam1, float)
-        self.assertEqual(23.3, rawTeamScoreTeam1)
-        self.assertEqual(92.3, rawTeamScoreTeam2)
-        self.assertEqual(63.8, rawTeamScoreTeam3)
+        self.assertEqual(23.33, rawTeamScoreTeam1)
+        self.assertEqual(92.34, rawTeamScoreTeam2)
+        self.assertEqual(63.77, rawTeamScoreTeam3)
 
     def test_getTeamSuccess(self):
         rawTeamSuccessTeam1 = SslCalculator(0.0, 0.0, 100.0, 100.0, 100.0, 1, 600.0).getTeamSuccess()
         rawTeamSuccessTeam2 = SslCalculator(4.5, 4.5, 550.0, 150.0, 100.0, 7, 3600.0).getTeamSuccess()
         rawTeamSuccessTeam3 = SslCalculator(4.5, 2.5, 550.0, 150.0, 100.0, 7, 3600.0).getTeamSuccess()
         self.assertIsInstance(rawTeamSuccessTeam1, float)
-        self.assertEqual(23.3, rawTeamSuccessTeam1)
-        self.assertEqual(92.3, rawTeamSuccessTeam2)
-        self.assertEqual(63.8, rawTeamSuccessTeam3)
+        self.assertEqual(23.33, rawTeamSuccessTeam1)
+        self.assertEqual(92.34, rawTeamSuccessTeam2)
+        self.assertEqual(63.77, rawTeamSuccessTeam3)
 
     def test_getTeamLuck(self):
         rawTeamLuckTeam1 = SslCalculator(1.0, 0.0, 100.0, 100.0, 100.0, 1, 600.0).getTeamLuck()
@@ -30,6 +30,6 @@ class TestSslCalculator(unittest.TestCase):
         self.assertIsInstance(rawTeamLuckTeam1, float)
         self.assertEqual(-100.0, rawTeamLuckTeam1)
         self.assertEqual(0.0, rawTeamLuckTeam2)
-        self.assertEqual(-28.5, rawTeamLuckTeam3)
+        self.assertEqual(-28.57, rawTeamLuckTeam3)
 
 
