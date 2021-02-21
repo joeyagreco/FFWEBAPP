0ef84877aed4a7c38c0797147784050b935648ee
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-20 19:36:29 -0600

    styling league stats page BROKEN STATE [cant update league stat dropdown]

--- static/javascript/leagueStatsPage.js
+++ static/javascript/leagueStatsPage.js
@@ -1,6 +1,9 @@
-function submitLeagueStat(pageNumber) {
+function submitLeagueStat(leagueStat) {
+    console.log(leagueStat);
+    console.log(typeof leagueStat);
+    alert(leagueStat);
     var leagueId = document.getElementById("league_id").value;
-    var leagueStat = document.getElementById("league_stat").value;
+//    var leagueStat = document.getElementById("user_selected_stat").value;
     window.location = "/league-stats?league_id="+leagueId+"&stat_selection="+leagueStat;
 }
 
