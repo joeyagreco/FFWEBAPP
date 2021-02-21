a17ee7070108bc5c2f96b827d2b65a55484e0810
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-20 18:30:15 -0600

    styling head to head stats dropdown

--- static/javascript/headToHeadStatsPage.js
+++ static/javascript/headToHeadStatsPage.js
@@ -1,6 +1,14 @@
-function getHeadToHeadTable() {
+function getHeadToHeadTable(team1, team2) {
     var leagueId = document.getElementById("league_id").value;
-    var team1 = document.getElementById("team1Id").value;
-    var team2 = document.getElementById("team2Id").value;
     window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2;
+}
+
+function updateTeam1(team1) {
+    var team2 = document.getElementById("team2button").value;
+    getHeadToHeadTable(team1, team2);
+}
+
+function updateTeam2(team2) {
+    var team1 = document.getElementById("team1button").value;
+    getHeadToHeadTable(team1, team2);
 }
