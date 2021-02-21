ae1603a15f72c9f2cce944dd1bb63d2e72d55e1b
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-20 19:39:32 -0600

    styling league stats page FIXED STATE

--- static/javascript/leagueStatsPage.js
+++ static/javascript/leagueStatsPage.js
@@ -1,9 +1,5 @@
 function submitLeagueStat(leagueStat) {
-    console.log(leagueStat);
-    console.log(typeof leagueStat);
-    alert(leagueStat);
     var leagueId = document.getElementById("league_id").value;
-//    var leagueStat = document.getElementById("user_selected_stat").value;
     window.location = "/league-stats?league_id="+leagueId+"&stat_selection="+leagueStat;
 }
 
