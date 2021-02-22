af2b1d8cec5cc9e9082d0bba5a2cf9c87f259d3b
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-21 20:00:05 -0600

    added bootstrap dropdown to stats explained pages

--- static/javascript/statsBase.js
+++ static/javascript/statsBase.js
@@ -1,7 +1,6 @@
-function submitStat() {
+function submitStat(stat) {
     var leagueId = document.getElementById("league_id").value;
-    var selectedStat = document.getElementById("stat_name").value;
-    window.location = "/stats-explained?league_id="+leagueId+"&selected_stat="+selectedStat;
+    window.location = "/stats-explained?league_id="+leagueId+"&selected_stat="+stat;
 }
 
 function reroute(statName) {
