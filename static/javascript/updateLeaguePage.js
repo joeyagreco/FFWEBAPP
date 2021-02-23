c09d0310e1521861e2cecbc1128891f30dd00e73
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-22 20:38:56 -0600

    added SWAL2 popup for deleting a year

--- static/javascript/updateLeaguePage.js
+++ static/javascript/updateLeaguePage.js
@@ -51,7 +51,21 @@ function addYear() {
 function deleteYear() {
     var leagueId = document.getElementById("league_id").value;
     var currentYear = document.getElementById("original_year_number").value;
-    window.location = "/delete-year?league_id="+leagueId+"&selected_year="+currentYear;
+
+        Swal.fire({
+    title: 'Are you sure you want to delete the ' + currentYear + ' year?',
+    text: "This cannot be undone.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete the ' + currentYear + 'year.',
+    heightAuto: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+             window.location = "/delete-year?league_id="+leagueId+"&selected_year="+currentYear;
+         }
+    })
 }
 
 function yearRedirect(year) {
