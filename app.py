c02929a0e869c9ec777440ab5314a17b04782c6a
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-24 10:16:40 -0600

    working on posting manually for update league

--- app.py
+++ app.py
@@ -94,10 +94,12 @@ def updateLeague():
         selectedYear = int(selectedYear)
         return render_template("updateLeaguePage.html", league=leagueOrError, selected_year=selectedYear)
     else:
+        print("posting")
         # we got a POST
         # convert the POST request headers into a python dictionary
         newDataStr = request.data.decode("UTF-8")
         newDataDict = ast.literal_eval(newDataStr)
+        print(newDataDict)
         leagueId = int(newDataDict["league_id"])
         # update league name
         leagueName = newDataDict["league_name"]
@@ -116,13 +118,16 @@ def updateLeague():
             for year in leagueOrError["years"].keys():
                 if year == yearNumber:
                     # user chose a year that is already in league
-                    return render_template("updateLeaguePage.html", league=leagueOrError, error_message="Year already exists in league.", selected_year=originalYear)
+                    print(1)
+                    return redirect(url_for("updateLeague", league_id=leagueId, year=yearNumber))
+                    # return render_template("updateLeaguePage.html", league=leagueOrError, error_message="Year already exists in league.", selected_year=originalYear)
         # update team names
         teams = []
         for teamId in range(1, numberOfTeams + 1):
             teams.append({"teamId": int(teamId), "teamName": newDataDict[f"team_{teamId}"]})
         if isinstance(leagueOrError, Error):
             # could not find league
+            print(1.5)
             return render_template("indexHomepage.html", error_message=leagueOrError.errorMessage())
         years = leagueOrError["years"]
         currentYear = years[originalYear]
@@ -145,12 +150,15 @@ def updateLeague():
         leagueOrError = mainController.getLeague(leagueId)
         if isinstance(leagueOrError, Error):
             # could not find league
+            print(2)
             return render_template("indexHomepage.html", error_message=leagueOrError.errorMessage())
         elif isinstance(updated, Error):
             # could not update league
+            print(3)
             return render_template("updateLeaguePage.html", league=leagueOrError, error_message=updated.errorMessage(), selected_year=originalYear)
         else:
             # successfully updated league
+            print(4)
             return redirect(url_for("updateLeague", league_id=leagueId, year=yearNumber))
 
 
