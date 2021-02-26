cd5ba9d39f9c819567914961b60a1595ee62f980
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-26 14:25:37 -0600

    added get plus minus test for years

--- test/test_StatCalculators/test_ScoresCalculator.py
+++ test/test_StatCalculators/test_ScoresCalculator.py
@@ -158,8 +158,20 @@ class TestScoresCalculator(unittest.TestCase):
         matchupList = [matchup1, matchup2, matchup3]
         week3 = WeekModel(3, matchupList)
         weekList = [week1, week2, week3]
-        year = YearModel(2020, teamList, weekList)
-        yearDict = {2020: year}
+        year2020 = YearModel(2020, teamList, weekList)
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        matchup1 = MatchupModel(1, team1, team2, 100.6, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 100.1, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week2 = WeekModel(2, matchupList)
+        weekList = [week1, week2]
+        year2021 = YearModel(2021, teamList, weekList)
+        yearDict = {2020: year2020, 2021: year2021}
         leagueModel = LeagueModel(123456, "test", 6, yearDict)
         plusMinusTeam1_1 = ScoresCalculator(1, leagueModel, [2020]).getPlusMinus(throughWeek=1)
         plusMinusTeam1_2 = ScoresCalculator(1, leagueModel, [2020]).getPlusMinus(throughWeek=2)
@@ -170,6 +182,8 @@ class TestScoresCalculator(unittest.TestCase):
         plusMinusTeam1_only1and3 = ScoresCalculator(1, leagueModel, [2020]).getPlusMinus(onlyWeeks=[1, 3])
         plusMinusTeam1_allParams = ScoresCalculator(1, leagueModel, [2020]).getPlusMinus(throughWeek=2, vsTeamIds=[2])
         plusMinusTeam1_default = ScoresCalculator(1, leagueModel, [2020]).getPlusMinus()
+        plusMinusTeam1_2021 = ScoresCalculator(1, leagueModel, [2021]).getPlusMinus()
+        plusMinusTeam1_bothYears = ScoresCalculator(1, leagueModel, [2020, 2021]).getPlusMinus()
         self.assertIsInstance(plusMinusTeam1_1, float)
         self.assertEqual(0, plusMinusTeam1_1)
         self.assertEqual(-1.1, plusMinusTeam1_2)
@@ -180,6 +194,8 @@ class TestScoresCalculator(unittest.TestCase):
         self.assertEqual(-1.2, plusMinusTeam1_only1and3)
         self.assertEqual(-1.1, plusMinusTeam1_allParams)
         self.assertEqual(-2.3, plusMinusTeam1_default)
+        self.assertEqual(-0.4, plusMinusTeam1_2021)
+        self.assertEqual(-2.7, plusMinusTeam1_bothYears)
 
     def test_getStandardDeviation(self):
         team1 = TeamModel(1, "team1")
