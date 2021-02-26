bc35828b259ee6327a5d3eae2292dc64d9994aa3
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-26 12:03:52 -0600

    working on head to head stats year support

--- static/javascript/headToHeadStatsPage.js
+++ static/javascript/headToHeadStatsPage.js
@@ -4,20 +4,24 @@
 //    window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2+"&year="+year;
 //}
 
-function updateTeam1(team1) {
-    var team2 = document.getElementById("team2button").value;
-    getHeadToHeadTable(team1, team2);
-}
+//function updateTeam1(team1) {
+//    var team2 = document.getElementById("team2button").value;
+//    getHeadToHeadTable(team1, team2);
+//}
+//
+//function updateTeam2(team2) {
+//    var team1 = document.getElementById("team1button").value;
+//    getHeadToHeadTable(team1, team2);
+//}
 
-function updateTeam2(team2) {
-    var team1 = document.getElementById("team1button").value;
-    getHeadToHeadTable(team1, team2);
-}
-
-function yearRedirect(year) {
+function yearRedirect(year, team1, team2) {
     var leagueId = document.getElementById("league_id").value;
-    var team1 = document.getElementById("team1button").value;
-    var team2 = document.getElementById("team2button").value;
+    if(!team1) {
+        var team1 = document.getElementById("team1button").value;
+    }
+    if(!team2) {
+        var team2 = document.getElementById("team2button").value;
+    }
     // GET request
      window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2+"&year="+year;
 }
