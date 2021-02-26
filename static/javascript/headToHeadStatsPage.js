5aa32d867cb92c14108179dd7ebe975accd5fbb7
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-26 11:54:49 -0600

    working on head to head stats year support

--- static/javascript/headToHeadStatsPage.js
+++ static/javascript/headToHeadStatsPage.js
@@ -1,7 +1,8 @@
-function getHeadToHeadTable(team1, team2) {
-    var leagueId = document.getElementById("league_id").value;
-    window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2;
-}
+//function getHeadToHeadTable(team1, team2) {
+//    var leagueId = document.getElementById("league_id").value;
+//    var year = document.getElementById("selectYearButton").value;
+//    window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2+"&year="+year;
+//}
 
 function updateTeam1(team1) {
     var team2 = document.getElementById("team2button").value;
@@ -15,6 +16,8 @@ function updateTeam2(team2) {
 
 function yearRedirect(year) {
     var leagueId = document.getElementById("league_id").value;
+    var team1 = document.getElementById("team1button").value;
+    var team2 = document.getElementById("team2button").value;
     // GET request
      window.location = "/head-to-head-stats?league_id="+leagueId+"&team1="+team1+"&team2="+team2+"&year="+year;
 }
