0900ee1f991a489e3c64f0872676948ae7d2f991
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-26 15:29:05 -0600

    added get smart wins test for years

--- test/test_StatCalculators/test_SmartCalculator.py
+++ test/test_StatCalculators/test_SmartCalculator.py
@@ -29,17 +29,35 @@ class TestSmartCalculator(unittest.TestCase):
         matchupList = [matchup1, matchup2, matchup3]
         week2 = WeekModel(2, matchupList)
         weekList = [week1, week2]
-        year = YearModel(2020, teamList, weekList)
-        yearDict = {2020: year}
+        year2020 = YearModel(2020, teamList, weekList)
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        matchup1 = MatchupModel(1, team1, team2, 100.6, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 100.1, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week2 = WeekModel(2, matchupList)
+        weekList = [week1, week2]
+        year2021 = YearModel(2021, teamList, weekList)
+        yearDict = {2020: year2020, 2021: year2021}
         leagueModel = LeagueModel(123456, "test", 6, yearDict)
-        smartCalculator = SmartCalculator(leagueModel, [2020])
-        smartWins1 = smartCalculator.getSmartWinsOfScore(100)
-        smartWins2 = smartCalculator.getSmartWinsOfScore(100)
-        smartWinsDefault = smartCalculator.getSmartWinsOfScore(100)
+        smartCalculator2020 = SmartCalculator(leagueModel, [2020])
+        smartCalculator2021 = SmartCalculator(leagueModel, [2021])
+        smartCalculatorBothYears = SmartCalculator(leagueModel, [2020, 2021])
+        smartWins1 = smartCalculator2020.getSmartWinsOfScore(100)
+        smartWins2 = smartCalculator2020.getSmartWinsOfScore(100)
+        smartWinsDefault = smartCalculator2020.getSmartWinsOfScore(100)
+        smartWins_2021 = smartCalculator2021.getSmartWinsOfScore(100)
+        smartWinsBothYears = smartCalculatorBothYears.getSmartWinsOfScore(100)
         self.assertIsInstance(smartWins1, float)
         self.assertEqual(0.32, smartWins1)
         self.assertEqual(0.32, smartWins2)
         self.assertEqual(0.32, smartWinsDefault)
+        self.assertEqual(0.18, smartWins_2021)
+        self.assertEqual(0.26, smartWinsBothYears)
 
     def test_getSmartWinsOfScoresList(self):
         team1 = TeamModel(1, "team1")
