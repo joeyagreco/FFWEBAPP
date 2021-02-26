fc5659b3f826d90490c2d7f43c2c9c290d3c62da
jg <57115019+joeyagreco@users.noreply.github.com>
2021-02-26 13:33:04 -0600

    added get losses test for years

--- test/test_StatCalculators/test_RecordCalculator.py
+++ test/test_StatCalculators/test_RecordCalculator.py
@@ -74,19 +74,31 @@ class TestRecordCalculator(unittest.TestCase):
         team5 = TeamModel(5, "team5")
         team6 = TeamModel(6, "team6")
         teamList = [team1, team2, team3, team4, team5, team6]
-        matchup1 = MatchupModel(1, team1, team2, 100.6, 100.5)
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
         matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
         matchup3 = MatchupModel(3, team5, team6, 104, 105)
         matchupList = [matchup1, matchup2, matchup3]
         week1 = WeekModel(1, matchupList)
-        matchup1 = MatchupModel(1, team1, team2, 100.4, 100.5)
+        matchup1 = MatchupModel(1, team1, team2, 100.6, 100.5)
         matchup2 = MatchupModel(2, team3, team4, 100.1, 100)
         matchup3 = MatchupModel(3, team5, team6, 104, 105)
         matchupList = [matchup1, matchup2, matchup3]
         week2 = WeekModel(2, matchupList)
         weekList = [week1, week2]
-        year = YearModel(2020, teamList, weekList)
-        yearDict = {2020: year}
+        year2020 = YearModel(2020, teamList, weekList)
+        matchup1 = MatchupModel(1, team1, team2, 100, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 0.0, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week1 = WeekModel(1, matchupList)
+        matchup1 = MatchupModel(1, team1, team2, 100.6, 100.5)
+        matchup2 = MatchupModel(2, team3, team4, 100.1, 100)
+        matchup3 = MatchupModel(3, team5, team6, 104, 105)
+        matchupList = [matchup1, matchup2, matchup3]
+        week2 = WeekModel(2, matchupList)
+        weekList = [week1, week2]
+        year2021 = YearModel(2021, teamList, weekList)
+        yearDict = {2020: year2020, 2021: year2021}
         leagueModel = LeagueModel(123456, "test", 6, yearDict)
         lossesTeam1_1 = RecordCalculator(1, leagueModel, [2020]).getLosses(throughWeek=1)
         lossesTeam1_2 = RecordCalculator(1, leagueModel, [2020]).getLosses(throughWeek=2)
@@ -96,15 +108,19 @@ class TestRecordCalculator(unittest.TestCase):
         lossesTeam1_only1and2 = RecordCalculator(1, leagueModel, [2020]).getLosses(onlyWeeks=[1, 2])
         lossesTeam1_allParams = RecordCalculator(1, leagueModel, [2020]).getLosses(throughWeek=1, vsTeamIds=[2])
         lossesTeam1_default = RecordCalculator(1, leagueModel, [2020]).getLosses()
+        lossesTeam1_2021 = RecordCalculator(1, leagueModel, [2021]).getLosses()
+        lossesTeam1_bothYears = RecordCalculator(1, leagueModel, [2020,2021]).getLosses()
         self.assertIsInstance(lossesTeam1_1, int)
-        self.assertEqual(0, lossesTeam1_1)
+        self.assertEqual(1, lossesTeam1_1)
         self.assertEqual(1, lossesTeam1_2)
         self.assertEqual(1, lossesTeam1_vs2)
         self.assertEqual(0, lossesTeam1_vs3)
-        self.assertEqual(1, lossesTeam1_only2)
+        self.assertEqual(0, lossesTeam1_only2)
         self.assertEqual(1, lossesTeam1_only1and2)
-        self.assertEqual(0, lossesTeam1_allParams)
+        self.assertEqual(1, lossesTeam1_allParams)
         self.assertEqual(1, lossesTeam1_default)
+        self.assertEqual(1, lossesTeam1_2021)
+        self.assertEqual(2, lossesTeam1_bothYears)
 
     def test_getTies(self):
         team1 = TeamModel(1, "team1")
