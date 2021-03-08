392be65b5241b44d8f91eaa9f5e8156b4478e16f
jg <57115019+joeyagreco@users.noreply.github.com>
2021-03-08 09:26:52 -0600

    working on adding owner comparisons to league stats

--- static/javascript/leagueStatsPage.js
+++ static/javascript/leagueStatsPage.js
@@ -6,10 +6,15 @@ function submitLeagueStat(year, leagueStat) {
     if(!leagueStat) {
         leagueStat = document.getElementById("stat_selection").value;
     }
+    // check if the selected stat is an "ALWAYS ALL TIME" stat
+    // TODO find a better way to do this
+    if(leagueStat == "Owner Comparison") {
+        year = 0;
+    }
     window.location = "/league-stats?league_id="+leagueId+"&stat_selection="+leagueStat+"&year="+year;
 }
 
-function prepYearDropdownForAllTime() {
+function lockYearDropdownToAllTime() {
     var yearDropdownElement = document.getElementById("select_year_button");
     yearDropdownElement.disabled = true;
     yearDropdownElement.classList.add("disabled");
