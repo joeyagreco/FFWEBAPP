190b001613b8a111a287e3a3115bc3a37fb6ca5e
jg <57115019+joeyagreco@users.noreply.github.com>
2021-03-09 13:55:50 -0600

    added github button link

--- static/javascript/aboutPage.js
+++ static/javascript/aboutPage.js
@@ -7,3 +7,8 @@ function venmoRedirect() {
     var redirectLink = "https://www.venmo.com/joey-a-greco";
     window.open(redirectLink, '_blank');
 }
+
+function githubRedirect() {
+    var redirectLink = "https://github.com/joeyagreco";
+    window.open(redirectLink, '_blank');
+}
