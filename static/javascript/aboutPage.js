5ee264ae88906efaf54febea124260af77f048c9
jg <57115019+joeyagreco@users.noreply.github.com>
2021-03-09 13:43:19 -0600

    adding support button links

---
+++ static/javascript/aboutPage.js
@@ -0,0 +1,9 @@
+function paypalRedirect() {
+    var redirectLink = "https://paypal.me/joeyagreco";
+    window.open(redirectLink, '_blank');
+}
+
+function venmoRedirect() {
+    var redirectLink = "https://www.venmo.com/joey-a-greco";
+    window.open(redirectLink, '_blank');
+}
