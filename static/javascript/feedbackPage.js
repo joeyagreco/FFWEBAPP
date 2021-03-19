76fb075df7f87aac843673375ee155a1e78a6cdf
jg <57115019+joeyagreco@users.noreply.github.com>
2021-03-18 20:19:39 -0500

    adding front end validation for feedback forms

--- static/javascript/feedbackPage.js
+++ static/javascript/feedbackPage.js
@@ -32,3 +32,58 @@ function validateForms(name, email, feedback) {
     }
     return null;
 }
+
+function activateSubmitButton() {
+    // this activates the submit button when forms are filled correctly
+    // get all values
+    var name = document.getElementById("name_form").value;
+    var email = document.getElementById("email_form").value;
+    var feedback = document.getElementById("feedback_form").value;
+
+    // set booleans of whether or not these fields are valid
+    var validName = isValidName(name);
+    var validEmail = isValidEmail(email);
+    var validFeedback = isValidFeedback(feedback);
+
+    var submitButtonElement = document.getElementById("send_feedback_button");
+    // check if all fields are valid
+    if(validName && validEmail && validFeedback) {
+        // activate submit button
+        submitButtonElement.classList.remove("disabled");
+    }
+    else {
+        // disable submit button
+        submitButtonElement.classList.add("disabled");
+    }
+}
+
+function isValidName(name) {
+    //returns a boolean of whether or not the given name (string) is valid
+    if(name.length == 0) {
+        return false;
+    }
+    return true;
+}
+
+function isValidEmail(email) {
+    //returns a boolean of whether or not the given email (string) is valid
+    if(email.length == 0) {
+        // email is not a required field, so it is valid if none is given
+        return true;
+    }
+    if(email.substring(email.length-4, email.length+1) != ".com") {
+        return false;
+    }
+    if(!email.includes("@")) {
+        return false;
+    }
+    return true;
+}
+
+function isValidFeedback(feedback) {
+    //returns a boolean of whether or not the given feedback (string) is valid
+    if(feedback.length == 0) {
+        return false;
+    }
+    return true;
+}
