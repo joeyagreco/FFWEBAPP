4e8c26ea350431fef645f4fcfecc898c8932851b
jg <57115019+joeyagreco@users.noreply.github.com>
2021-03-26 19:45:03 -0500

    fixed table styling and now always sort by win %

--- static/javascript/teamStatsPage.js
+++ static/javascript/teamStatsPage.js
@@ -5,11 +5,22 @@ function yearRedirect(year) {
      window.location = "/team-stats?league_id="+leagueId+"&year="+year;
 }
 
+function getIndexOfColumnToSort() {
+    // this returns the index for the column for Win Percentage
+    var index = 4;
+    var year = document.getElementById("selectYearButton").value;
+    if(year == 0) {
+        // win percentage is in the 5th index
+        index = 5;
+    }
+    return index;
+}
+
 function initializeTable() {
     $(document).ready( function () {
         $('#statsTable').DataTable(
             {
-                "order": [[ 4, "desc" ]],
+                "order": [[ getIndexOfColumnToSort(), "desc" ]],
                 "searching": false
             }
         );
