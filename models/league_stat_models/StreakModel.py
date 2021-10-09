73fddb213e04b2f7887a99f9b8ed1c7797c3f3b2
jg <57115019+joeyagreco@users.noreply.github.com>
2021-10-09 12:35:59 -0500

    updated streak model to dataclass

--- models/league_stat_models/StreakModel.py
+++ models/league_stat_models/StreakModel.py
@@ -1,31 +1,14 @@
+from dataclasses import dataclass
+
+from models.league_models.TeamModel import TeamModel
+
+
+@dataclass
 class StreakModel:
-
-    def __init__(self, **stats):
-        self.__owner = stats["owner"]
-        self.__streakNumber = stats["streakNumber"]
-        self.__firstDate = stats["firstDate"]
-        self.__firstTeam = stats["firstTeam"]
-        self.__lastDate = stats["lastDate"]
-        self.__lastTeam = stats["lastTeam"]
-        self.__ongoing = stats["ongoing"]
-
-    def getOwner(self):
-        return self.__owner
-
-    def getStreakNumber(self):
-        return self.__streakNumber
-
-    def getFirstDate(self):
-        return self.__firstDate
-
-    def getFirstTeam(self):
-        return self.__firstTeam
-
-    def getLastDate(self):
-        return self.__lastDate
-
-    def getLastTeam(self):
-        return self.__lastTeam
-
-    def getOngoing(self):
-        return self.__ongoing
+    firstDate: str
+    firstTeam: TeamModel
+    lastDate: str
+    lastTeam: TeamModel
+    ongoing: bool
+    owner: TeamModel
+    streakNumber: int
